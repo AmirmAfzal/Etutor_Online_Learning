7590b3d05d1518e29d3383bf26cbcd30753b9b3c
user25256998549589 <gojesabzkhoshk@gmail.com>
2025-07-14 16:25:07 +0330

    initial config

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-
 const HomePage = () => {
   return (
     <div>
-      
+      <h1>Learn with expert anytime anywhere</h1>
+      <a href="" className="link link-primary">
+        Hi
+      </a>
+      <button className="btn btn-primary">Click here</button>
+      <button className="btn btn-secondary">Click here</button>
+      <button className="btn btn-neutral">click here</button>
+      <div className="bg-base-100 mt-10 w-full border-2 p-10">
+        <div className="bg-base-200 w-full p-10">
+          <div className="bg-base-300 h-10 w-full p-10"></div>
+        </div>
+      </div>
     </div>
   );
 };
