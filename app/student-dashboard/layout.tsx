4534ddcc1f5d26ada0658a768b57ec53a03f33b4
amirAghayari <amiraghayari2119@gmail.com>
2025-07-16 10:16:45 +0330

    update layout (student dashboard)

--- app/student-dashboard/layout.tsx
+++ app/student-dashboard/layout.tsx
@@ -5,6 +5,7 @@ import { Icon } from "@iconify/react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import StudentProfile from "@/components/StudentProfile";
 
 const mockProfiles = [
   {
@@ -39,35 +40,12 @@ export default function StudentDashboardLayout({
         <div className="w-full max-w-5xl">
           {/* Profile Card */}
           {mockProfiles.map((profile, idx) => (
-            <div
+            <StudentProfile
               key={idx}
-              className="bg-base-100 border-primary/20 relative mt-8 mb-0 flex flex-col items-center gap-4 border-2 p-4 sm:gap-6 sm:p-6 md:flex-row md:items-center"
-            >
-              <Image
-                src={profile.image}
-                alt="Profile"
-                width={112}
-                height={112}
-                className="h-20 w-20 rounded-full object-cover sm:h-28 sm:w-28"
-                style={{ width: "auto", height: "auto" }}
-                priority={true}
-              />
-              <div className="flex flex-1 flex-col items-center text-center md:items-start md:text-left">
-                <h2 className="text-base-content text-lg font-bold sm:text-2xl">
-                  {profile.name}
-                </h2>
-                <p className="text-base-content/50 mt-2 text-xs sm:mt-4 sm:text-base">
-                  {profile.job}
-                </p>
-              </div>
-              <button className="btn btn-soft btn-primary mt-2 w-full gap-2 font-bold md:static md:top-6 md:right-6 md:mt-0 md:ml-auto md:w-auto">
-                Become Instructor
-                <Icon
-                  icon="solar:arrow-right-outline"
-                  className="text-xl sm:text-2xl"
-                />
-              </button>
-            </div>
+              name={profile.name}
+              job={profile.job}
+              image={profile.image}
+            />
           ))}
           {/* Tabs (Dynamic Links) */}
           <div className="bg-base-100 border-primary/20 mb-0 flex justify-between overflow-x-auto border-2 border-t-0 px-6 py-2">
