628cb5733eb5fb50b217df6aa57ba4a5dd66e81e
amirAghayari <amiraghayari2119@gmail.com>
2025-07-22 22:30:32 +0330

    Update not-found page (initial)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,36 @@
-"use client"
-import Link from 'next/link';
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const ErrorPage = () => {
-    return (
-        <div className='w-full h-screen flex flex-col items-center justify-center'>
-            <h1 className="text-7xl font-audiowide text-error">404</h1>
-            <p className="">Page not found</p> 
-            <Link href={"/"} className="btn btn-primary rounded-full px-8 mt-8">Back to Home Page</Link>
-        </div>
-    )
-}
+  return (
+    <div className="flex h-screen w-full flex-row items-center justify-center">
+      <div className="ml-52 flex w-1/2 flex-col items-start gap-4">
+        <h1 className="font-audiowide text-base-content/20 text-7xl font-bold">
+          Error 404
+        </h1>
+        <span className="text-3xl font-bold">Oops! Page not found</span>
+        <p className="text-base-content/60">
+          Something went wrong. It’s look that your requested <br /> could not
+          be found. It's look like the link is broken or the <br /> page is
+          removed.
+        </p>
 
-export default ErrorPage
+        <Link href={"/"} className="btn btn-primary mt-8 px-8">
+          Back to Home Page
+        </Link>
+      </div>
+      <div className="1/2">
+        <Image
+          src={"/images/notFound.png"}
+          alt="not found"
+          width={1024}
+          height={900}
+          className="w-full"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
