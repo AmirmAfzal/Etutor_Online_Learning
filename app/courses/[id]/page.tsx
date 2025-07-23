ac27a3b5a2fc950a9e36305952b0e795e4b4c591
amirAghayari <amiraghayari2119@gmail.com>
2025-07-23 23:23:20 +0330

    Update Single course page (Curriculum with shadcn)

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -1,4 +1,11 @@
 import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Icon from "@/components/ui/Icon";
 
 // Fake data that would come from a database
 
@@ -45,6 +52,50 @@ For example, this is a Design course but I don't teach you Photoshop. Because Ph
     "A computer with internet access",
     "Willingness to learn and practice",
     "No prior design or coding experience required",
+  ],
+  curriculum: [
+    {
+      title: "Getting Started",
+      lectures: 4,
+      duration: "51m",
+      content: [
+        { title: "What's is Webflow?", info: "07:31" },
+        { title: "Sign up in Webflow", info: "07:31" },
+        { title: "Webflow Terms & Conditions", info: "5.3 MB" },
+        { title: "Teaser of Webflow", info: "07:31" },
+        { title: "Practice Project", info: "5.3 MB" },
+      ],
+    },
+    {
+      title: "Secret of Good Design",
+      lectures: 52,
+      duration: "5h 49m",
+      content: [],
+    },
+    {
+      title: "Practice Design Like an Artist",
+      lectures: 43,
+      duration: "53m",
+      content: [],
+    },
+    {
+      title: "Web Development (webflow)",
+      lectures: 137,
+      duration: "10h 6m",
+      content: [],
+    },
+    {
+      title: "Secrets of Making Money Freelancing",
+      lectures: 21,
+      duration: "38m",
+      content: [],
+    },
+    {
+      title: "Advanced",
+      lectures: 39,
+      duration: "91m",
+      content: [],
+    },
   ],
 };
 
@@ -180,7 +231,60 @@ export default function CoursePage() {
                 </div>
               </div>
 
-              {/* shadcn */}
+              {/* TODO: Add icon  */}
+              <div className="mt-12 w-full">
+                <span className="text-base-content/80 mb-4 block text-2xl font-semibold">
+                  Curriculum
+                </span>
+                <Accordion type="single" collapsible className="w-full">
+                  {fakeCourses.curriculum.map((section, index) => (
+                    <AccordionItem
+                      key={index}
+                      value={`section-${index + 1}`}
+                      className="bg-base-100 border-base-content/10 border transition-all duration-150 hover:translate-y-[-1px]"
+                    >
+                      <AccordionTrigger className="min-h-[72px] px-6">
+                        <div className="flex w-full flex-row items-start justify-between gap-3">
+                          <span className="text-base-content/80 font-semibold">
+                            {section.title}
+                          </span>
+                          <div className="flex flex-wrap gap-5">
+                            <span className="text-base-content/60 flex items-center gap-2 text-sm">
+                              <Icon
+                                icon="ph:play-circle-duotone"
+                                className="text-secondary"
+                              />
+                              {section.lectures} Lectures
+                            </span>
+                            <span className="text-base-content/60 flex items-center gap-2 text-sm">
+                              <Icon
+                                icon="ph:clock"
+                                className="text-primary text-lg"
+                              />
+                              {section.duration}
+                            </span>
+                          </div>
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        <ul className="px-4">
+                          {section.content.map((item, idx) => (
+                            <li
+                              key={idx}
+                              className="flex items-center gap-2 py-1 text-sm"
+                            >
+                              {item.title}
+                              <span className="ml-auto text-xs">
+                                {item.info}
+                              </span>
+                            </li>
+                          ))}
+                        </ul>
+                      </AccordionContent>
+                    </AccordionItem>
+                  ))}
+                </Accordion>
+              </div>
             </div>
           </div>
         </div>
