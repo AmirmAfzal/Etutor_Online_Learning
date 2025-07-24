f3632301f35883ce5a4532ea7a4252b8c9a9bf8a
amirAghayari <amiraghayari2119@gmail.com>
2025-07-24 12:23:46 +0330

    Add TruncatedText components

---
+++ components/Courses/TruncatedText.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
+const TruncatedText = ({
+  text,
+  maxLength,
+}: {
+  text: string;
+  maxLength: number;
+}) => {
+  const isTruncated = text.length > maxLength;
+  const displayedText = isTruncated ? text.slice(0, maxLength) + "..." : text;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
+      <span className="text-base-content/70 text-sm">
+        {isExpanded ? text : displayedText}
+      </span>
+      <button
+        onClick={() => setIsExpanded(!isExpanded)}
+        className="text-base-content/80 font-semibold"
+      >
+        {isExpanded ? "Show Less" : "Read More"}
+      </button>
+    </>
+  );
+};
+export default TruncatedText;
