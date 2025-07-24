d7f8bd5b86c4a1e76ea4af3fb7a3968590711c1f
amirAghayari <amiraghayari2119@gmail.com>
2025-07-24 12:22:19 +0330

    Update SingleCourse (Add Instructors )

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -6,6 +6,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import Icon from "@/components/ui/Icon";
+import TruncatedText from "@/components/Courses/TruncatedText";
 
 // Fake data that would come from a database
 
@@ -98,6 +99,29 @@ For example, this is a Design course but I don't teach you Photoshop. Because Ph
     },
   ],
 };
+
+const Instructors = [
+  {
+    name: "John Doe",
+    bio: "Web Design Expert",
+    avatar: "/images/instructors/instructor-1.png",
+    students: 354355,
+    rating: 4.8,
+    courses: 12,
+    description:
+      "John is a seasoned web designer with over 10 years of experience in creating stunning websites. He specializes in Figma and Webflow, helping students turn their design ideas into reality.",
+  },
+  {
+    name: "Jane Smith",
+    bio: "Figma Specialist",
+    avatar: "/images/instructors/instructor-2.png",
+    students: 254321,
+    rating: 4.9,
+    courses: 8,
+    description:
+      "Jane is a talented Figma designer with a passion for creating user-friendly interfaces. She has worked with various clients to bring their visions to life.",
+  },
+];
 
 export default function CoursePage() {
   return (
@@ -285,6 +309,60 @@ export default function CoursePage() {
                   ))}
                 </Accordion>
               </div>
+            </div>
+
+            <div className="mt-12 w-full space-y-3">
+              <span className="text-base-content/80 mb-4 block text-2xl font-semibold">
+                Course instructors{` (${Instructors.length})`}
+              </span>
+              {Instructors.map((instructor, index) => (
+                <div key={index} className="border-base-300 w-full border p-4">
+                  <div className="flex flex-row items-center gap-4">
+                    <Image
+                      width={64}
+                      height={64}
+                      src={instructor.avatar}
+                      alt={instructor.name}
+                      className="border-base-300 mb-6 w-2/5 rounded-full border"
+                    />
+                    <div className="flex flex-col gap-2">
+                      <span className="text-base-content/80 font-semibold">
+                        {instructor.name}
+                      </span>
+                      <span className="text-base-content/60 text-sm">
+                        {instructor.bio}
+                      </span>
+
+                      <div className="text-base-content/80 flex flex-row items-center justify-between gap-4 text-sm font-semibold">
+                        <span>
+                          {instructor.rating}
+                          <span className="text-base-content/60 ml-1">
+                            Course rating
+                          </span>
+                        </span>
+                        <span>
+                          {instructor.students.toLocaleString()}
+                          <span className="text-base-content/60 ml-1">
+                            Students
+                          </span>
+                        </span>
+                        <span>
+                          {instructor.courses}
+                          <span className="text-base-content/60 ml-1">
+                            Courses
+                          </span>
+                        </span>
+                      </div>
+                      <div className="mt-4">
+                        <TruncatedText
+                          text={instructor.description}
+                          maxLength={150}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
