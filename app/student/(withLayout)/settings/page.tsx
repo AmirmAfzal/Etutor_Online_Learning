b5d28b900bee6740ca634b14621d252e82479548
amirAghayari <amiraghayari2119@gmail.com>
2025-08-09 11:50:20 +0330

    fix StudentDashboard's  lint error

--- app/student/(withLayout)/settings/page.tsx
+++ app/student/(withLayout)/settings/page.tsx
@@ -1,23 +1,23 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
 import AccountSettingsForm from "@/components/Student/student-settings/AccountSettingsForm";
 import PasswordSettingsForm from "@/components/Student/student-settings/PasswordSettingsForm";
 import { authOptions } from "@/lib/auth/authOptions";
 import { connectDB } from "@/lib/db/db";
 import studentModel, { StudentInterface } from "@/lib/db/models/studentModel";
 import userModel, { UserInterface } from "@/lib/db/models/userModel";
-import _ from "lodash";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 const StudentSettingsPage = async () => {
-  const userId = "USER_ID_FROM_AUTH";
   await connectDB();
   const session = await getServerSession(authOptions);
   if (!session) return redirect("/auth/signin");
+
   const foundUser: UserInterface | null = await userModel.findOne({
     _id: session?.user.id,
   });
-
   if (!foundUser) return redirect("/auth/signin");
+
   const foundStudent: StudentInterface | null = await studentModel.findOne({
     user: foundUser._id,
   });
