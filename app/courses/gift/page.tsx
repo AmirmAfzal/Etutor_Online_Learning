f0b58a91fbbeab8ae66a11340cb3c96713827ea7
amirAghayari <amiraghayari2119@gmail.com>
2025-08-11 11:01:59 +0330

    Add Recipient's Information to Gift's page with shadcn , reactHookForm (initial)

---
+++ app/courses/gift/page.tsx
@@ -0,0 +1,52 @@
+import GiftCourseForm from "@/components/Courses/gift-course/GiftCourseForm";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { SlashIcon } from "lucide-react";
+
+const GiftCoursePage = () => {
+  return (
+    <section>
+      <div className="bg-base-200 flex w-screen flex-col items-center justify-center gap-4 py-8">
+        <h3 className="text-xl font-semibold">Gift Course</h3>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <SlashIcon />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/courses">Courses</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <SlashIcon />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbLink>Complete web...</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <SlashIcon />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                href="/courses/gift"
+                className="text-base-content/80"
+              >
+                Gift Courses
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <GiftCourseForm />
+    </section>
+  );
+};
+
+export default GiftCoursePage;
---
+++ components/Courses/gift-course/GiftCourseForm.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const GiftCourseFormSchema = z.object({
+  name: z.string().min(1, "Recipient's name is required"),
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  message: z.string().optional(),
+});
+
+const GiftCourseForm = () => {
+  const form = useForm<z.infer<typeof GiftCourseFormSchema>>({
+    resolver: zodResolver(GiftCourseFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof GiftCourseFormSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <h3 className="mb-4 text-xl font-semibold">Gift course</h3>
+      <span className="text-md font-medium">Recipient's Information</span>
+
+      <Form {...form}>
+        <form
+          className="mt-4 w-1/3 space-y-6"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          {/* Name */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-base-content/80 mb-2 text-xs font-medium">
+                  Recipient's Name
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Full name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Email */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-base-content/80 mb-2 text-xs font-medium">
+                  Recipient's Email
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Email Address" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Message */}
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-base-content/80 mb-2 text-xs font-medium">
+                  Gift Message
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="pt-6 pb-20"
+                    placeholder="Add your personal message here..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default GiftCourseForm;
