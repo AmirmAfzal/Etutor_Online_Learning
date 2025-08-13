ebb6f57faec4c985449ce473b963bc3769215fc4
amirAghayari <amiraghayari2119@gmail.com>
2025-08-13 11:19:53 +0330

    Update student/message responsive (initial-2)

--- components/Student/student-messages/MessageHeader.tsx
+++ components/Student/student-messages/MessageHeader.tsx
@@ -28,7 +28,7 @@ const MessageHeader = ({
           <Button
             variant="default"
             size="sm"
-            className="!btn !btn-primary z-10 !rounded-full md:!rounded-none"
+            className="!btn !btn-primary z-10 !rounded-full !shadow-md md:!rounded-none md:!shadow-none"
           >
             <Icon icon="ph:plus" />
             Compose
@@ -38,11 +38,11 @@ const MessageHeader = ({
           <DialogPrimitive.Overlay className="bg-base-content/50 fixed inset-0 z-40" />
           <DialogPrimitive.Content className="bg-base-100 fixed top-1/2 left-1/2 z-50 w-full max-w-xl -translate-x-1/2 -translate-y-1/2 p-6">
             <div className="border-base-content/20 flex items-center justify-between border-b pb-2">
-              <span className="text-base-content/70 text-lg font-semibold">
+              <span className="text-base-content/70 text-md font-semibold md:text-lg">
                 New Message
               </span>
               <DialogPrimitive.Close asChild>
-                <button className="btn btn-ghost text-base-content/70 text-2xl">
+                <button className="btn btn-ghost text-base-content/70 text-xl md:text-2xl">
                   &times;
                 </button>
               </DialogPrimitive.Close>
