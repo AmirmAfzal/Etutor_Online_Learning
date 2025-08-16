593d525468d9c7588d013fdee30dc69883f98cd6
amirAghayari <amiraghayari2119@gmail.com>
2025-08-16 10:21:13 +0330

    courseFilter's child components

---
+++ components/Courses/courseFilter/Duration.tsx
@@ -0,0 +1,50 @@
+//
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const Duration = ({ duration }: { duration: { [key: string]: number } }) => {
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className="border-base-300 mt-4 border"
+    >
+      <AccordionItem value="duration">
+        <AccordionTrigger className="border-base-300 rounded-none border border-x-0 px-2 text-lg font-semibold">
+          DURATION
+        </AccordionTrigger>
+        <AccordionContent className="mt-2">
+          {Object.entries(duration).map(
+            ([duration, i]) =>
+              duration && (
+                <div
+                  key={duration}
+                  className="text-base-content/70 flex items-center justify-between px-2 text-sm"
+                >
+                  <form className="flex flex-row items-center gap-2 p-2">
+                    <input
+                      type="checkbox"
+                      id={duration}
+                      className="checkbox checkbox-primary checkbox-xs"
+                    />
+                    <span className="text-base-content/80 text-xs font-medium">
+                      {duration}
+                    </span>
+                  </form>
+                  <span className="text-base-content/60 text-xs font-medium">
+                    {i.toLocaleString()}
+                  </span>
+                </div>
+              )
+          )}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default Duration;
---
+++ components/Courses/courseFilter/Rating.tsx
@@ -0,0 +1,56 @@
+//
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Icon from "@/components/ui/Icon";
+
+const Rating = ({
+  rating,
+}: {
+  rating: {
+    label: string;
+    count: number;
+  }[];
+}) => {
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      className="border-base-300 mt-4 border"
+    >
+      <AccordionItem value="rating">
+        <AccordionTrigger className="border-base-300 rounded-none border border-x-0 px-2 text-lg font-semibold">
+          RATING
+        </AccordionTrigger>
+        <AccordionContent>
+          {rating.map((item, i) => (
+            <form
+              key={i}
+              className="flex flex-row items-center justify-between gap-2 p-2"
+            >
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  id={item.label}
+                  className="checkbox checkbox-primary checkbox-xs"
+                />
+                <Icon icon="ph:star-fill" className="text-primary" />
+                <span className="text-base-content/80 text-xs font-medium">
+                  {item.label}
+                </span>
+              </div>
+              <span className="text-base-content/70">
+                {item.count.toLocaleString()}
+              </span>
+            </form>
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default Rating;
