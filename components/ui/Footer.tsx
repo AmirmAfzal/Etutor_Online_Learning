024779fa1d3321060bc3fa2e59de53e283a0c469
amir-hosein-003 <ah.kanani003@gmail.com>
2025-08-27 11:40:17 +0330

    add Conditional footer display

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,38 +1,70 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 import Icon from "./Icon";
 
 const Footer = () => {
+  const pathname = usePathname();
+
   return (
     <footer className="bg-neutral">
-      <div className="border-base-300/60 border-b">
-        <div className="text-base-100/80 container mx-auto flex flex-col items-center gap-32 px-8 py-16 md:flex-row md:px-0">
-          <div className="flex flex-col gap-6">
-            <h3 className="max-w-md text-2xl font-bold">
-              Start learning with 67.1k students around the world.
-            </h3>
-            <span className="flex flex-col gap-2 md:flex-row">
-              <button className="btn btn-primary">Join The Family</button>
-              <button className="btn btn-soft">Browse All Course</button>
-            </span>
-          </div>
-          <div className="flex flex-row items-center justify-center gap-8 md:gap-16">
-            <span>
-              <p className="text-base-100 text-3xl font-bold">6.3K</p>
-              <p className="text-base-300/60 text-sm">Online Courses</p>
-            </span>
-            <span>
-              <p className="text-base-100 text-3xl font-bold">26K</p>
-              <p className="text-base-300/60 text-sm">Certified Instructor</p>
-            </span>
-            <span>
-              <p className="text-base-100 text-3xl font-bold">99.0%</p>
-              <p className="text-base-300/60 text-sm">Success Rate</p>
-            </span>
+      {pathname === "/" || pathname === "/category" ? (
+        <div className="border-base-300/60 border-b">
+          <div className="text-base-100/80 container mx-auto flex flex-col items-center gap-32 px-8 py-16 md:flex-row md:px-0">
+            <div className="flex flex-col gap-6">
+              <h3 className="max-w-md text-2xl font-bold">
+                Start learning with 67.1k students around the world.
+              </h3>
+              <span className="flex flex-col gap-2 md:flex-row">
+                <button className="btn btn-primary">Join The Family</button>
+                <button className="btn btn-soft">Browse All Course</button>
+              </span>
+            </div>
+            <div className="flex flex-row items-center justify-center gap-8 md:gap-16">
+              <span>
+                <p className="text-base-100 text-3xl font-bold">6.3K</p>
+                <p className="text-base-300/60 text-sm">Online Courses</p>
+              </span>
+              <span>
+                <p className="text-base-100 text-3xl font-bold">26K</p>
+                <p className="text-base-300/60 text-sm">Certified Instructor</p>
+              </span>
+              <span>
+                <p className="text-base-100 text-3xl font-bold">99.0%</p>
+                <p className="text-base-300/60 text-sm">Success Rate</p>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
+      {pathname === "/become-instructor" && (
+        <div className="border-base-300/60 border-b">
+          <div className="text-base-100 container mx-auto grid grid-cols-1 gap-16 px-8 py-16 md:grid-cols-2 md:gap-32 md:px-0">
+            <div className="space-y-6 md:space-y-8">
+              <h3 className="text-3xl font-bold">
+                Start teaching with us and inspire others
+              </h3>
+              <p className="text-base-300/60">
+                Become an instructor & start teaching with 26k certified
+                instructors. Create a success story with 67.1k Students — Grow
+                yourself with 71 countries.
+              </p>
+              <button className="btn btn-primary">Register Now</button>
+            </div>
+            <div>
+              <Image
+                src="/images/Union-footer.png"
+                alt="union footer"
+                width={500}
+                height={500}
+              />
+            </div>
+          </div>
+        </div>
+      )}
       <div className="border-base-300/60 border-b">
         <div className="text-base-100/80 container mx-auto grid grid-cols-1 gap-8 px-8 py-16 md:grid-cols-4 md:px-0">
           <div className="col-span-1 space-y-4">
