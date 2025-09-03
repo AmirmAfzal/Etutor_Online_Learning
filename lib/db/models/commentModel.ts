160414efa1a88ed5d0dd2e1a1a941d0c3e135349
amirAghayari <amiraghayari2119@gmail.com>
2025-09-03 13:32:02 +0330

    Create commentModel.ts  (initial)

---
+++ lib/db/models/commentModel.ts
@@ -0,0 +1,25 @@
+import mongoose, {ObjectId, Schema , Document} from "mongoose";
+
+export interface CommentInterface extends Document {
+    comment: string;
+    refPath: string[];
+    lecture: ObjectId;
+    title: string;
+    avatar: string;
+    replies:ObjectId[]
+}
+
+const commentSchema = new Schema<CommentInterface , Document>({
+    comment : {type: String , required: true },
+    refPath : [{type: String, required: true }],
+    title : {type: String , required: true },
+    avatar : {type: String , required: true },
+    lecture : {type: Schema.Types.ObjectId ,ref:"lecture", required: true },
+    replies : [{type: Schema.Types.ObjectId ,ref:"replies", }],
+},{
+    timestamps : true,
+})
+
+const commentModel = mongoose.models.comment || mongoose.model<CommentInterface>("Comment", commentSchema);
+
+export default commentModel;
