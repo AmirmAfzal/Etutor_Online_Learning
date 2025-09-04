c220f1fa3556d30257e35ac539322f52a1feed75
amirAghayari <amiraghayari2119@gmail.com>
2025-09-04 22:15:46 +0330

    update commentModel & Add replyModel

--- lib/db/models/commentModel.ts
+++ lib/db/models/commentModel.ts
@@ -1,25 +1,34 @@
-import mongoose, {ObjectId, Schema , Document} from "mongoose";
+import mongoose, { ObjectId, Schema, Document } from "mongoose";
 
 export interface CommentInterface extends Document {
-    comment: string;
-    refPath: string[];
-    lecture: ObjectId;
-    title: string;
-    avatar: string;
-    replies:ObjectId[]
+  comment: string;
+  refPath: "Student" | "Instructor" | "Admin";
+  lecture: ObjectId;
+  title: string;
+  avatar: string;
+  replies: ObjectId[];
 }
 
-const commentSchema = new Schema<CommentInterface , Document>({
-    comment : {type: String , required: true },
-    refPath : [{type: String, required: true }],
-    title : {type: String , required: true },
-    avatar : {type: String , required: true },
-    lecture : {type: Schema.Types.ObjectId ,ref:"lecture", required: true },
-    replies : [{type: Schema.Types.ObjectId ,ref:"replies", }],
-},{
-    timestamps : true,
-})
+const commentSchema = new Schema<CommentInterface, Document>(
+  {
+    comment: { type: String, required: true },
+    refPath: {
+      type: String,
+      required: true,
+      enum: ["Student", "Instructor", "Admin"],
+    },
+    title: { type: String, required: true },
+    avatar: { type: String, required: true },
+    lecture: { type: Schema.Types.ObjectId, ref: "lecture", required: true },
+    replies: [{ type: Schema.Types.ObjectId, ref: "replies" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const commentModel = mongoose.models.comment || mongoose.model<CommentInterface>("Comment", commentSchema);
+const commentModel =
+  mongoose.models.comment ||
+  mongoose.model<CommentInterface>("Comment", commentSchema);
 
 export default commentModel;
---
+++ lib/db/models/replyModel.ts
@@ -0,0 +1,33 @@
+import mongoose, { ObjectId, Schema, Document } from "mongoose";
+
+export interface ReplyInterface extends Document {
+  reply: string;
+  refPath: "Student" | "Instructor" | "Admin";
+  user: ObjectId;
+  comment: ObjectId;
+  title: string;
+  avatar: string;
+}
+
+const replySchema = new Schema<ReplyInterface, Document>(
+  {
+    reply: { type: String, required: true },
+    refPath: {
+      type: String,
+      required: true,
+      enum: ["Student", "Instructor", "Admin"],
+    },
+    user: { type: Schema.Types.ObjectId, refPath: "refPath", required: true },
+    comment: { type: Schema.Types.ObjectId, ref: "Comment", required: true },
+    title: { type: String, required: true },
+    avatar: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const replyModel =
+  mongoose.models.reply || mongoose.model<ReplyInterface>("Reply", replySchema);
+
+export default replyModel;
