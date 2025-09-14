e849d04c5346e1025b9285244f08fec9455a619d
user25256998549589 <104265017+AmirmAfzal@users.noreply.github.com>
2025-09-14 12:56:07 +0330

    fixed build error

--- components/Courses/watchCourses/CreateComment.tsx
+++ components/Courses/watchCourses/CreateComment.tsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 import { useActionState, useRef, useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Form from "next/form";
-import { useSearchParams } from "next/navigation";
+
 
 import {
   Dialog,
