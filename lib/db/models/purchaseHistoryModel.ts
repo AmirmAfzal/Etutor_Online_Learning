53b16b365bc314906dd8c541761f7390cd6611e4
amirAghayari <amiraghayari2119@gmail.com>
2025-09-16 10:16:57 +0330

    Update purchaseHistoryModel.ts

--- lib/db/models/purchaseHistoryModel.ts
+++ lib/db/models/purchaseHistoryModel.ts
@@ -2,12 +2,19 @@ import mongoose, { Document, ObjectId , Schema } from "mongoose";
 
 export interface PurchaseHistoryInterface extends Document {
   courses: ObjectId[];
+  priceAtPurchase?: number;
+  currency?: string;
+  userId?: ObjectId;
 }
 
 const purchaseHistorySchema = new Schema<PurchaseHistoryInterface, Document>(
   {
-    courses: [{ type: Schema.Types.ObjectId, ref: "Course", required: true }],
+    courses: [{ type: Schema.Types.ObjectId, ref: "course", required: true }],
+  priceAtPurchase: { type: Schema.Types.ObjectId, ref: "PriceAtPurchase", required: true },
+  currency: { type: String, required: true, default: "USD" },
+  userId: { type: Schema.Types.ObjectId, ref: "user", required: true },
   },
+
   {
     timestamps: true,
   }
