335ef59df01be1ee244e2801c834d8947447a0bb
amirAghayari <amiraghayari2119@gmail.com>
2025-09-20 11:37:10 +0330

    Create courseProgressModel.ts

---
+++ lib/db/models/courseProgressModel.ts
@@ -0,0 +1,31 @@
+import mongoose, { ObjectId, Schema, Document } from "mongoose";
+
+export interface CourseProgressInterface extends Document {
+  user: ObjectId;
+  course: ObjectId;
+  lecture: ObjectId;
+  completed: boolean;
+  completedAt?: Date;
+}
+
+const courseProgressSchema = new Schema<CourseProgressInterface & Document>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    course: { type: Schema.Types.ObjectId, ref: "Course", required: true },
+    lecture: { type: Schema.Types.ObjectId, ref: "Lecture", required: true },
+    completed: { type: Boolean, default: false },
+    completedAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const courseProgressModel =
+  mongoose.models.courseProgress ||
+  mongoose.model<CourseProgressInterface>(
+    "courseProgress",
+    courseProgressSchema
+  );
+
+export default courseProgressModel;
