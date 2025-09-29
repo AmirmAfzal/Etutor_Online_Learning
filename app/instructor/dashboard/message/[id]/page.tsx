448f86fe0f806cb58191ca01f63108242807afc1
amirAghayari <amiraghayari2119@gmail.com>
2025-09-29 11:15:40 +0330

    Update : sign in for chat in instructor dashboard

--- app/instructor/dashboard/message/[id]/page.tsx
+++ app/instructor/dashboard/message/[id]/page.tsx
@@ -1,7 +1,10 @@
 import ChatMessages from "@/components/Student/student-messages/ChatMessages";
 import ContactList from "@/components/Student/student-messages/ContactList";
-import MessageInput from "@/components/Student/student-messages/MessageInput";
 import { Types } from "mongoose";
+import { redirect } from "next/navigation";
+import { connectDB } from "@/lib/db/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth/authOptions";
 
 interface Props {
   params: Promise<{ id: Types.ObjectId & string }>;
@@ -11,12 +14,18 @@ const MessagesById = async (props: Props) => {
   const params = await props.params;
   const receiverId = params.id;
 
+  await connectDB();
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.id) {
+    redirect("/auth/signin");
+  }
+
   return (
     <div className="border-base-300 bg-base-200 flex h-screen w-full gap-6 border p-6">
       {/* Contact List */}
       <div
-        className={`border-base-300 bg-base-100 flex flex-col border-r 
-        ${receiverId ? "hidden md:flex md:w-1/3" : "flex w-full md:w-1/3"}`}
+        className={`border-base-300 bg-base-100 flex flex-col border-r ${receiverId ? "hidden md:flex md:w-1/3" : "flex w-full md:w-1/3"}`}
       >
         <ContactList role="instructor" />
       </div>
@@ -24,8 +33,7 @@ const MessagesById = async (props: Props) => {
       {/* Chat Section */}
       {receiverId ? (
         <div
-          className={`bg-base-100 flex flex-col 
-          ${receiverId ? "flex w-full md:w-2/3" : "hidden md:flex md:w-2/3"}`}
+          className={`bg-base-100 flex flex-col ${receiverId ? "flex w-full md:w-2/3" : "hidden md:flex md:w-2/3"}`}
         >
           <div className="flex-1 overflow-y-auto">
             <ChatMessages receiverId={receiverId} />
@@ -33,7 +41,7 @@ const MessagesById = async (props: Props) => {
           {/*<MessageInput receiverId={receiverId} />*/}
         </div>
       ) : (
-        <div className="bg-base-100 text-base-content/70 hidden md:flex md:w-2/3 items-center justify-center">
+        <div className="bg-base-100 text-base-content/70 hidden items-center justify-center md:flex md:w-2/3">
           <p>Please select a student from the list to start a conversation.</p>
         </div>
       )}
